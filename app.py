c5ab2bacea6a8ed06dc2ee80ee959f490d2c44c1
Anjelie8 <25-13391@g.batstate-u.edu.ph>
2026-05-17 12:44:51 +0800

    Fixed missing send_file import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from flask import Flask, render_template, request, session, redirect, url_for, flash, send_file 
 import sqlite3
-import random
 import os
+import random
 import pandas as pd
 
 app = Flask(__name__)
